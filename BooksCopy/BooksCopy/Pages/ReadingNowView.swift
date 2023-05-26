038b6db8f2aa8715442ee45f06496751096fa07d
Luis Silva <luissilva@10-40-48-103.0200.ecps.br.iosda.org>
2023-05-26 08:19:20 -0300

    Atualizando tela

--- BooksCopy/BooksCopy/Pages/ReadingNowView.swift
+++ BooksCopy/BooksCopy/Pages/ReadingNowView.swift
@@ -13,61 +13,116 @@ struct ReadingNowView: View {
         UINavigationBar.appearance().titleTextAttributes = [.font : UIFont(name: "Georgia-Bold", size:17)!]
     }
     
-    @State private var pick = 0
+    let uiscreen = UIScreen.main.bounds
+    
+    @Environment(\.colorScheme) private var colorScheme
     
     var body: some View {
+        
         NavigationView {
             ScrollView{
+                //Text("Leando aora")
+                  //  .navigationBarTitleDisplayMode(.inline)
+                
+                
                 HStack{
                     Text("Today's reading")
-                        .colorMultiply(/*@START_MENU_TOKEN@*/Color(red: 0.017, green: 0.607, blue: 0.857)/*@END_MENU_TOKEN@*/)
+                        .foregroundColor(Color(red: 0.017, green: 0.607, blue: 0.857))
                     
                     Text("5 minutes left")
                 }.font(.system(size: 12.5))
                     .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.leading, 16)
+                    .padding(.leading, 32)
                 
                 VStack{
                     Divider()
-                        .padding(.horizontal, 16.0)
+                        .padding(.horizontal, 32)
                     
-                    Text("Current")
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.leading, 16)
-                    .font(.custom("Georgia-Bold", size: 18))
-                    
-                    HStack{
+                    ZStack{
+                        Rectangle()
+                                .fill(
+                                    LinearGradient(gradient: Gradient(colors: [first_color_correct, second_color]),
+                                                   startPoint: .top,
+                                                   endPoint: .bottom))
+                                .frame(width: self.uiscreen.width,
+                                       height: 300,
+                                       alignment: .center)
                         
-                        Button() {
+                        
+                        VStack{
+                            Text("Current")
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .padding(.leading, 32)
+                            .font(.custom("Georgia-Bold", size: 18))
                             
-                        }label: {
-                            Image("image 1")
-                              .resizable()
-                              .scaledToFit()
-                              .frame(maxHeight: 232)
-                              .padding(.leading, 16)
-                        }
-                      VStack{
-                        Text("The Swift Programming Language (Swift 5.7)")
-                              .padding(.leading,16)
-                              .font(.system(size:16))
-                          Text("")
-                          Text("1%")
-                              .padding(.leading,-78)
-                              .font(.system(size:12))
-                              .colorMultiply(.gray)
-                          Text("")
-                          MenuComponent()
-                        }
-                      }
-                    
-                    
+                            HStack{
+                                
+                                Button() {
+                                    
+                                }label: {
+                                    Image("image 1")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(maxHeight: 212)
+                                        .padding(.leading, 1)
+                                }
+                                Text("   ")
+                                VStack{
+                                    Text("The Swift Programming Language (Swift 5.7)")
+                                        .font(.system(size:14))
+                                    Text("")
+                                    Text("1%")
+                                        .padding(.leading,-78)
+                                        .font(.system(size:12))
+                                        .colorMultiply(.gray)
+                                    Text("")
+                                    MenuComponent()
+                                }.padding(.trailing, 2)
+                            }
+                        }.padding(.top, -30)
+                    }
+                    .padding(.top, -1)
                     
                 }
             }.navigationBarTitle(Text("Reading Now").font(.subheadline),displayMode: .large)
         }
             
     }
+    
+    private var first_color: Color{
+        switch colorScheme{
+        case .light:
+            return Color(red: 0.996, green: 0.996, blue: 0.996)
+        case .dark:
+            return /*@START_MENU_TOKEN@*/Color(red: 0.118, green: 0.118, blue: 0.118)/*@END_MENU_TOKEN@*/
+        @unknown default:
+            return /*@START_MENU_TOKEN@*/Color(red: 0.0, green: 0.0, blue: 0.0)/*@END_MENU_TOKEN@*/
+        }
+        
+    }
+    private var first_color_correct: Color{
+        switch colorScheme{
+        case .light:
+            return Color(red: 0.996, green: 0.996, blue: 0.996)
+        case .dark:
+            return /*@START_MENU_TOKEN@*/Color(red: 0.0, green: 0.0, blue: 0.0)/*@END_MENU_TOKEN@*/
+        @unknown default:
+            return /*@START_MENU_TOKEN@*/Color(red: 0.0, green: 0.0, blue: 0.0)/*@END_MENU_TOKEN@*/
+        }
+        
+    }
+    private var second_color: Color{
+        switch colorScheme{
+        case .light:
+            return Color(red: 0.941, green: 0.941, blue: 0.941)
+        case .dark:
+            return /*@START_MENU_TOKEN@*/Color(red: 0.0, green: 0.0, blue: 0.0)/*@END_MENU_TOKEN@*/
+        @unknown default:
+            return /*@START_MENU_TOKEN@*/Color(red: 0.0, green: 0.0, blue: 0.0)/*@END_MENU_TOKEN@*/
+        }
+        
+    }
+    
 }
 
 struct ReadingNowView_Previews: PreviewProvider {
