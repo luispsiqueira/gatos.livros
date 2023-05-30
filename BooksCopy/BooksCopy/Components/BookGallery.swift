1b8ac02f5fbdd2956ae02bbbc046a332987c4c56
Luis Silva <luissilva@Luiss-MacBook-Pro.local>
2023-05-30 10:53:01 -0300

    Readind Now finalizado

--- BooksCopy/BooksCopy/Components/BookGallery.swift
+++ BooksCopy/BooksCopy/Components/BookGallery.swift
@@ -37,9 +37,9 @@ struct BookGallery: View {
                     ForEach(0..<data.count, id: \.self) { item in
                         
                         if item % 2 == 0 {
-                            BookComponent(data[item]).padding(.trailing, 30)
+                            BookComponent(data[item],250).padding(.trailing, 30)
                         } else {
-                            BookComponent(data[item])
+                            BookComponent(data[item],250)
                                 .padding(.leading, 30)
                         }
                     }
