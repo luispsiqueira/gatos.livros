f6bfc9580b43d55bf7acad1c9855b4a98185bcc1
caio melloni <caiomelloni.contato@gmail.com>
2023-05-31 19:49:56 -0300

    adiciona tab de ler livros

---
+++ BooksCopy/BooksCopy/Pages/ReadBook.swift
@@ -0,0 +1,32 @@
+//
+//  Read.swift
+//  BooksCopy
+//
+//  Created by Caio Melloni dos Santos on 31/05/23.
+//
+
+import SwiftUI
+struct ReadBook: View {
+    
+    var body: some View {
+        TabView {
+            BookPage {
+                Text("😎")
+            }
+            BookPage {
+                Text("🥰")
+            }
+            BookPage {
+                Text("A")
+            }
+        }
+        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+        .ignoresSafeArea()
+    }
+}
+
+struct Read_Previews: PreviewProvider {
+    static var previews: some View {
+        ReadBook()
+    }
+}
